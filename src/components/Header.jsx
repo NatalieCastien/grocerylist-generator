a1163a717e37a2047318907e65438560f63f6722
unknown <natalie.castien@gmail.com>
2020-04-26 18:35:37 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Header() {
     return (
     <header>
-        <h1>Create your own grocerylist</h1>
+        <h1>Create your ownnnn grocerylist</h1>
     </header>
     )
 }
