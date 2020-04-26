179b940621ccf216c63696a59e94ac959c0c5c77
unknown <natalie.castien@gmail.com>
2020-04-26 18:43:15 +0200

    Updates

---
+++ precache-manifest.e6971f25400015985a91fa97272e1a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb18bc1f048ad4b914e2bb065df27611",
+    "url": "/grocerylist-generator/index.html"
+  },
+  {
+    "revision": "c33fce79ca65ab42f3b9",
+    "url": "/grocerylist-generator/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "c33fce79ca65ab42f3b9",
+    "url": "/grocerylist-generator/static/js/2.6b4276ca.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/grocerylist-generator/static/js/2.6b4276ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5561946e9eec546bcc8d",
+    "url": "/grocerylist-generator/static/js/main.526391e8.chunk.js"
+  },
+  {
+    "revision": "feb78ecf3e98425e0cf5",
+    "url": "/grocerylist-generator/static/js/runtime-main.3228c6f3.js"
+  }
+]);
